67be7065fdcfb91afe370ac6ac2f393e6ccd7cc2
guptadevendra1987 <guptadevendra1987@gmail.com>
2025-03-08 18:05:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraformstate"  
+    storage_account_name = "sgterraformstate"                      
+    container_name       = "tfstate"                       
+    key                  = "state.terraform.tfstate"       
+  }
+}
 
